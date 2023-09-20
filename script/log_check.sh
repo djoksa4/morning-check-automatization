c5db7c6f05464058e1780b9131db3435cb0c98f8
djoksa4 <vodovardjordje.cavps@gmail.com>
2023-09-21 00:08:04 +0200

    script edit 1

--- script/log_check.sh
+++ script/log_check.sh
@@ -4,7 +4,7 @@
 directory="/home/adminuser/morning-check-automatization"
 
 # List files in the directory and then count them
-file_count=$(find "$directory" -type f | wc -l)
+file_count=$(find "$directory" -maxdepth 1 -type f | wc -l)
 
 # Output number of files
 echo "Number of files in $directory: $file_count"
