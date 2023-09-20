64598eb57cd5e60e6319553ecc61f7d733e0c9ce
djoksa4 <vodovardjordje.cavps@gmail.com>
2023-09-20 23:50:18 +0200

    initial commit

---
+++ script/log_check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Specify the directory you want to count files in
+directory="/home/adminuser/morning-check-automatization"
+
+# List files in the directory and then count them
+file_count=$(find "$directory" -type f | wc -l)
+
+# Output the number of files
+echo "Number of files in $directory: $file_count"
