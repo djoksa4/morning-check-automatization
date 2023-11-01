1779845d5b4be3f27d500baeb42ce7beea0c3f46
djoksa4 <vodovardjordje.cavps@gmail.com>
2023-11-01 11:16:43 +0100

    rework1.2

--- script/log_check.sh
+++ script/log_check.sh
@@ -28,26 +28,30 @@ echo "$current_datetime - $front_sys_file_count Frontend SYS files found and del
 # Move all the app log files older than 1 day to archive
 echo "$current_datetime - Moving app log files older than 1 day to archive..." >> "$log_file"
 moved_files_count=0
-find "$app_logs_directory" -type f ! -newermt $(date +%Y-%m-%d) -exec mv {} "$archive_directory" \; -exec sh -c 'moved_files_count=$((moved_files_count + 1))' \;
+find "$app_logs_directory" -type f ! -newermt $(date +%Y-%m-%d) \( -name 'EMS*' -o -name 'NOTIF*' -o -name 'Scheduler*' -o -name '*.gz' \) -exec mv {} "$archive_directory" \; -exec sh -c 'moved_files_count=$((moved_files_count + 1))' \;
 echo "$current_datetime - Found and moved $moved_files_count app log files to archive." >> "$log_file"
 
+
+
+
+
 # Check the number of files in the archive AFTER the move from app_logs_dir
-new_archive_file_count=$(find "$archive_directory" -maxdepth 1 -type f | wc -l)
+archive_file_count=$(find "$archive_directory" -maxdepth 1 -type f | wc -l)
 
 # If there are more than 10,000 files in the archive_directory after the move, log an error and exit
-if [ "$new_archive_file_count" -gt 10000 ]; then
-    echo "$current_datetime - Error: Unexpectedly large amount of files present in the archive ($new_archive_file_count files). Aborting." >> "$log_file"
+if [ "$archive_file_count" -gt 10000 ]; then
+    echo "$current_datetime - Error: Unexpectedly large amount of files present in the archive ($archive_file_count files). Aborting." >> "$log_file"
     echo "====================================================================" >> "$log_file"
     exit 1
 else
     # Print the number of files present in the archive_directory
-    echo "$current_datetime - Total of $new_archive_file_count app log files found in the archive directory."
+    echo "$current_datetime - Total of $archive_file_count app log files found in the archive directory. Deleting files older than 7 days..."
     
     # Delete files with timestamps older than 7 days in the archive_directory
     find "$archive_directory" -type f -mtime +7 -exec rm {} \;
     
     # Print the new number of files after deletion
-    newest_archive_file_count=$(find "$archive_directory" -maxdepth 1 -type f | wc -l)
-    echo "$current_datetime - Total of $newest_archive_file_count app log files left in the archive directory after deleting logs older than 7 days."
+    new_archive_file_count=$(find "$archive_directory" -maxdepth 1 -type f | wc -l)
+    echo "$current_datetime - Total of $new_archive_file_count app log files left in the archive directory after deletion."
     echo "====================================================================" >> "$log_file"
 fi
